8f182818919951b5929f4526e855d129f9acbf23
forest-shadow <mamasay@yandex.ru>
2020-03-04 21:15:22 +0300

    Add basic `TaskAddForm` component

--- src/components/tasks/TaskAddButton/TaskAddForm.tsx
+++ src/components/tasks/TaskAddButton/TaskAddForm.tsx
@@ -1,8 +1,167 @@
 import React from 'react';
+import {
+  Typography,
+  TextField,
+  Box,
+  FormControl,
+  InputLabel,
+  Select,
+  Button
+} from '@material-ui/core';
+import { Formik, FormikErrors } from 'formik';
+import { makeStyles } from '@material-ui/styles';
 
 const TaskAddForm = () => {
-  return <div>2345</div>;
+  const classes = useStyles();
+  return (
+    <>
+      <Box mb={2.5}>
+        <Typography variant="h6" component="h3">
+          Add New Task
+        </Typography>
+      </Box>
+      <Formik
+        initialValues={
+          {
+            name: '',
+            description: '',
+            priority: '',
+            timeToComplete: ''
+          } as { [index: string]: string }
+        }
+        validate={values => {
+          const errors: FormikErrors<any> = {};
+          Object.keys(values).forEach((key: string) =>
+            !values[key] ? (errors[key] = 'Required') : null
+          );
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box mb={3}>
+              <TextField
+                required
+                label="Name"
+                variant="filled"
+                placeholder="Enter Name"
+                autoFocus
+                fullWidth
+                name="name"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.name}
+              />
+              {errors.name && touched.name && errors.name}
+            </Box>
+
+            <Box mb={3}>
+              <TextField
+                required
+                label="Description"
+                placeholder="Enter Description"
+                multiline
+                rows="4"
+                variant="filled"
+                fullWidth
+                name="description"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.description}
+              />
+              {errors.description && touched.description && errors.description}
+            </Box>
+
+            <Box mb={3}>
+              <FormControl variant="filled" fullWidth>
+                <InputLabel htmlFor="filled-priority">Priority</InputLabel>
+                <Select
+                  required
+                  native
+                  value={values.priority}
+                  onChange={handleChange}
+                  inputProps={{
+                    name: 'priority',
+                    id: 'filled-priority'
+                  }}
+                  placeholder="Select Priority"
+                >
+                  <option value="" />
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+                  <option value={3}>3</option>
+                </Select>
+              </FormControl>
+              {errors.priority && touched.priority && errors.priority}
+            </Box>
+
+            <Box mb={3}>
+              <TextField
+                required
+                label="Time To Complete"
+                variant="filled"
+                placeholder="Enter Time To Complete"
+                fullWidth
+                name="timeToComplete"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.timeToComplete}
+              />
+              {errors.timeToComplete &&
+                touched.timeToComplete &&
+                errors.timeToComplete}
+            </Box>
+
+            <Box display="flex" flexDirection="row-reverse">
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                disabled={isSubmitting}
+                className={classes.addBtn}
+              >
+                Add Task
+              </Button>
+            </Box>
+          </form>
+        )}
+      </Formik>
+    </>
+  );
 };
+
+const useStyles = makeStyles({
+  addBtn: {
+    color: '#FFFFFF',
+    border: '1px solid #0a588c',
+    backgroundColor: '#0a588c',
+
+    '&:hover': {
+      backgroundColor: '#0a588c',
+      borderColor: '#0a588c'
+    },
+
+    '&:active': {
+      backgroundColor: '#0a588c',
+      borderColor: '#0a588c'
+    }
+  }
+});
 
 interface Props {
   isOpen: boolean;
--- src/components/tasks/TaskAddButton/TaskAddTooltip.tsx
+++ src/components/tasks/TaskAddButton/TaskAddTooltip.tsx
@@ -15,22 +15,25 @@ const TaskAddTooltip = ({ history, children }: Props) => {
 
   return (
     <ClickAwayListener onClickAway={handleTooltipClose}>
-      <Tooltip
-        PopperProps={{
-          disablePortal: true
-        }}
-        arrow
-        classes={classes}
-        onClose={handleTooltipClose}
-        placement="right"
-        open={isOpen}
-        disableFocusListener
-        disableHoverListener
-        disableTouchListener
-        title={<TaskAddForm />}
-      >
-        {children}
-      </Tooltip>
+      <div>
+        <Tooltip
+          PopperProps={{
+            disablePortal: true
+          }}
+          arrow
+          interactive
+          classes={classes}
+          onClose={handleTooltipClose}
+          placement="right"
+          open={isOpen}
+          disableFocusListener
+          disableHoverListener
+          disableTouchListener
+          title={<TaskAddForm />}
+        >
+          {children}
+        </Tooltip>
+      </div>
     </ClickAwayListener>
   );
 };
@@ -44,14 +47,23 @@ interface OwnProps {
 const useStyles = makeStyles({
   arrow: {
     color: '#FFFFFF',
+    marginLeft: '-14px!important',
+
+    '&:before': {
+      marginTop: '-4px',
+      borderWidth: '10px 14px 10px 0!important'
+    }
   },
   tooltip: {
+    left: '5px',
     backgroundColor: '#FFFFFF',
     color: 'rgba(0, 0, 0, 0.87)',
-    maxWidth: 220,
+    width: '420px',
+    maxWidth: 420,
     fontSize: '12px',
     border: '1px solid #dadde9',
-    boxShadow: '0px 3px 7px 3px rgba(50, 50, 50, 0.35)',
+    filter: 'drop-shadow(2px 3px 3px rgba(50, 50, 50, 0.55))',
+    padding: '19px 16px 16px'
   }
 });
 
